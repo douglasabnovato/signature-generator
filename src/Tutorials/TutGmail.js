a39c1b40119a0ee070945c470b415a4fc6e3f889
@douglasabnovato <douglasabnovato@gmail.com>
2020-01-10 12:12:58 -0300

    routers

---
+++ src/Tutorials/TutGmail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TutGmail extends React.Component {
+    render(){
+        return(
+            <div>
+                Gmail
+            </div>
+        );
+    }
+}
---
+++ src/Tutorials/TutOutlook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class TutOutlook extends React.Component {
+    render(){
+        return(
+            <div>
+                testando
+            </div>
+        );
+    }
+}
